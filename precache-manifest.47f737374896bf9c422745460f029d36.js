3bd7e3701f29f5352aad43c29f77a4ff8f5bd79f
Kirill Medvedev <k-medvedev@yandex-team.ru>
2020-02-23 16:59:05 +0500

    Updates

---
+++ precache-manifest.47f737374896bf9c422745460f029d36.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da6195adba0a43a539db2f50860d4a0a",
+    "url": "/octomen/xo_front/index.html"
+  },
+  {
+    "revision": "14312ec9f8644211b50e",
+    "url": "/octomen/xo_front/static/css/main.8de66f8b.chunk.css"
+  },
+  {
+    "revision": "b4b98e1b37465bc458e8",
+    "url": "/octomen/xo_front/static/js/2.5f982b7f.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/octomen/xo_front/static/js/2.5f982b7f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14312ec9f8644211b50e",
+    "url": "/octomen/xo_front/static/js/main.5fd0b79b.chunk.js"
+  },
+  {
+    "revision": "36f6ad1f32baca1fe7f1",
+    "url": "/octomen/xo_front/static/js/runtime-main.521b6291.js"
+  },
+  {
+    "revision": "7cfa0804d95d59a374e3291f6c1b515c",
+    "url": "/octomen/xo_front/static/media/game.7cfa0804.png"
+  }
+]);
